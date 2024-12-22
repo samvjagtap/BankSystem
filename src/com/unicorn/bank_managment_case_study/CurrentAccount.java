59596761e21518f79c240c865fcd045399d08fca
itsamol2003 <amolpawar001213@gmail.com>
2024-12-22 19:21:29 +0530

    Added loan and Current accout classes

--- src/com/unicorn/bank_managment_case_study/CurrentAccount.java
+++ src/com/unicorn/bank_managment_case_study/CurrentAccount.java
@@ -1,5 +1,55 @@
 package com.unicorn.bank_managment_case_study;
 
-public class CurrentAccount {
 
+public class Current extends Account{
+
+	double overdraftLimit;
+
+	public Current() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public Current(long accountNumber, String accountHolderName, double balance, Date openingDate, Date closeingDate,
+			String accountType, boolean isActive) {
+		super(accountNumber, accountHolderName, balance, openingDate, closeingDate, accountType, isActive);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public double getOverdraftLimit() {
+		return overdraftLimit;
+	}
+
+	public void setOverdraftLimit(double overdraftLimit) {
+		this.overdraftLimit = overdraftLimit;
+	}
+
+	@Override
+	public double calculateInterest() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	public void withdrow(double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void deposite(double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 }
--- src/com/unicorn/bank_managment_case_study/LoanAccount.java
+++ src/com/unicorn/bank_managment_case_study/LoanAccount.java
@@ -1,5 +1,35 @@
 package com.unicorn.bank_managment_case_study;
 
-public class LoanAccount {
 
+import java.sql.Date;
+
+public class Loan  extends Account {
+
+	String accountHolderName;
+	double amountloan;
+	public Loan() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Loan(long accountNumber, String accountHolderName, double balance, Date openingDate, Date closeingDate,
+			String accountType, boolean isActive) {
+		super(accountNumber, accountHolderName, balance, openingDate, closeingDate, accountType, isActive);
+		// TODO Auto-generated constructor stub
+	}
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+	public double getAmountloan() {
+		return amountloan;
+	}
+	public void setAmountloan(double amountloan) {
+		this.amountloan = amountloan;
+	}
+	
+	
+	
+	
 }
